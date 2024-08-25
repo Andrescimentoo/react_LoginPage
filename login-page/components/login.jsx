4042b318cb322063e63600457855d2378e4551f3
Andre Nascimento <1914andre@gmail.com>
2024-08-25 18:48:28 -0300

    feat: adicionando elemento input de gmail ao projeto

--- login-page/components/login.jsx
+++ login-page/components/login.jsx
@@ -5,6 +5,9 @@ const login = () => {
     <div class="container" >
       <form>
             <h1>Acesse o sistema</h1>
+            <div>
+                <input type="email" placeholder='insira seu gmail' />
+            </div>
       </form>
     </div>
   )
