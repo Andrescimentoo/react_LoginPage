40defea531108a95f5679c231709455ff687d4c3
Andre Nascimento <1914andre@gmail.com>
2024-08-25 18:35:38 -0300

    feat: adicionando estrutura base do projeto formulario:

---
+++ login-page/components/login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div class="container" >
+      <form>
+            <h1>Acesse o sistema</h1>
+      </form>
+    </div>
+  )
+}
+
+export default login
