1bcfad3f930084b32a78be5dda2ab27ab6b87944
Andre Nascimento <1914andre@gmail.com>
2024-08-26 10:39:21 -0300

    Feat:adicionando icones nos inputs

--- login-page/components/login.jsx
+++ login-page/components/login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { FaUser,FaLock } from "react-icons/fa"
 
  const Login = () => {
   return (
@@ -7,10 +7,13 @@ import React from 'react'
             <h1>Acesse o sistema</h1>
             <div class = "email">
                 <input type="email" placeholder='insira seu gmail' />
+                <FaUser class="icons"/>
             </div>
             <div class="pass">
                 <input type="password" placeholder='insira sua senha'/>
+                <FaLock class="icon"/>
             </div>
+            <button>Entrar</button>
       </form>
     </div>
   )
