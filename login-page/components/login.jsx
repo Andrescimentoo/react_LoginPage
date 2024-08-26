e01e6f147af5de4d98d86da35033009fed0b378c
Andre Nascimento <1914andre@gmail.com>
2024-08-26 11:56:55 -0300

    feat:comecando a estruturar validacao do form,criando variaveis de armazenameto de validacoes e fixando pagina com o methodo preventDefault

--- login-page/components/login.jsx
+++ login-page/components/login.jsx
@@ -1,9 +1,32 @@
+//[PASSO 1] Vamos simular uma requisiçao para simular o login do user
+//para isso vamos usar o import state
+//o  state ele vai ser resposnavel por guardar todas as informaçoes que forem armazenadas nos inputs email e password 
+
+//[PASSO 2] Vamos criar entao duas variaveis,uma que vai consultar o valor e a outra que vai alterar o valor!
+//vamos criar uma funçao para envio do formulario
+//depois de criar a funcao temos que conectar ela ao formulario pra isso usamos a propriedade onSubmit que vai receber nossa funcao 
+
+//[PASSO 3] Na funcao temos q usar um callback "event" para travar o recarregamento da pagina ao enviar o form 
+//depois essa funcao vai receber o methodo preventDefault assim travamos o recarregamento automatico
+//assim monitoramos os dados quye passarao pelo nosso formulario
+
 import { FaUser,FaLock } from "react-icons/fa"
+import { useState } from "react" // importaçao resposanvel por manipular os estados dos elementos
+import "./login.css"
 
  const Login = () => {
+  
+  const[username,setUsername] = useState ("") //username le,setUsername altera
+  const[password,setPassword] = useState("")// password le, setUsername altera
+  const enviarForm = (event) => { //se enviarmos sem o parametro "event" ao clicar em enviar a pagina sempre vai recarregar
+    event.preventDefault()
+    console.log("formulario enviado com sucesso!")
+  } 
+
+
   return (
     <div class="container">
-      <form>
+      <form onSubmit={enviarForm}>
             <h1>Acesse o sistema</h1>
             <div class = "email">
                 <input type="email" placeholder='insira seu gmail' />
