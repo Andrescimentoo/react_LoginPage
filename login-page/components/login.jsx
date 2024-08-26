96ae43cad46ad596e007b48e6691dd395b904d4d
Andre Nascimento <1914andre@gmail.com>
2024-08-25 22:04:50 -0300

    feat:adicionando base de input de password

--- login-page/components/login.jsx
+++ login-page/components/login.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
-const login = () => {
+ const Login = () => {
   return (
-    <div class="container" >
+    <div class="container">
       <form>
             <h1>Acesse o sistema</h1>
-            <div>
+            <div class = "email">
                 <input type="email" placeholder='insira seu gmail' />
+            </div>
+            <div class="pass">
+                <input type="password" placeholder='insira sua senha'/>
             </div>
       </form>
     </div>
   )
 }
-
-export default login
+ export default Login
